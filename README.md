4f70c69eee6bc4cdeb024b1f9060e09ee66aaf90
MohammedAlghazali <hamood-monzer11@hotmail.com>
2020-10-12 16:39:46 +0300

    Reuse a Component example

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # React-Docs
 
-## check the branches to see the content
+## Reuse a Component
+
+Commonly, you might want to display React components in multiple places on the HTML page. Here is an example that displays the “Like” button three times and passes some data to it
+
+## Note
+
+This strategy is mostly useful while React-powered parts of the page are isolated from each other. Inside React code, it’s easier to use [component composition](https://reactjs.org/docs/components-and-props.html#composing-components) instead.
---
+++ like_button.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// here is the source of this code https://gist.github.com/gaearon/faa67b76a6c47adbab04f739cba7ceda
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked comment number ' + this.props.commentID;
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+// Find all DOM containers, and render Like buttons into them.
+document.querySelectorAll('.like_button_container')
+  .forEach(domContainer => {
+    // Read the comment ID from a data-* attribute.
+    const commentID = parseInt(domContainer.dataset.commentid, 10);
+    ReactDOM.render(
+      e(LikeButton, { commentID: commentID }),
+      domContainer
+    );
+  });
